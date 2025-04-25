48575454845268a53e889f7a504a4b97530ceda0
HungTran170904 <tienhung17092004@gmail.com>
2025-04-25 16:51:36 +0700

    docs: add a comment

--- src/main/java/meetingteam/meetingservice/MeetingServiceApplication.java
+++ src/main/java/meetingteam/meetingservice/MeetingServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(ServiceUrlConfig.class)
 public class MeetingServiceApplication {
-    // Statr application method
+    // Comment some thing
     public static void main(String[] args) {
         SpringApplication.run(MeetingServiceApplication.class, args);
     }
